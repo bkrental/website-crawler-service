7aa3c187e3e1b95db55ae692bfbeb61dada7c67c
Minh Phước <106443311+VideyKohaku@users.noreply.github.com>
2024-05-07 18:37:31 +0700

    [Feat] add csv export pipeline and init predict model (#1)
    * [Feat] add csv export pipeline and init predict model
    
    * [Feat] add models for price predicting
    
    * [Feat] added API server
    
    * [Feat] changed api endpoint

--- website_scraper/pipelines.py
+++ website_scraper/pipelines.py
@@ -1,6 +1,8 @@
 import re
 import requests
 from itemadapter import ItemAdapter
+import csv
+import re
 
 
 class MogiPipeline:
@@ -81,9 +83,18 @@ class MogiPipeline:
     def parse_address(self, address):
         address_details = address.strip().split(", ")
 
+        district_pattern = r"(Quận (2|9|Thủ Đức))( \(TP\.? Thủ Đức\))?"
+        province = address_details[-1]
+        district = address_details[-2]
+
+        if re.match(district_pattern, address_details[-2]):
+            province = "TP. Thủ Đức"
+            district = re.search(district_pattern, address_details[-2]).group(1)
+            print(district)
+
         return {
-            "province": address_details[-1],
-            "district": address_details[-2],
+            "province": province,
+            "district": district,
             "ward": address_details[-3],
             "street": address_details[-4],
         }
@@ -96,3 +107,60 @@ class MogiPipeline:
             return int(x.group(1))
 
         return None
+
+
+class CSVExportPipeline(MogiPipeline):
+    def __init__(self):
+        self.file = open("mogi_after_parsing.csv", "w", newline="", encoding="utf-8")
+        self.keys = [
+            "title",
+            "description",
+            "property_type",
+            "transaction_type",
+            "price",
+            "province",
+            "district",
+            "ward",
+            "street",
+            "location_latitude",
+            "location_longitude",
+            "owner_name",
+            "owner_contact",
+            "area",
+            "bedrooms",
+            "bathrooms",
+        ]
+        self.dict_writer = csv.DictWriter(self.file, fieldnames=self.keys)
+        self.dict_writer.writeheader()
+
+    def process_item(self, item, spider):
+        adapter = ItemAdapter(item)
+
+        print(f"item details: {adapter}")
+
+        json_data = {
+            "title": adapter["title"],
+            "description": adapter["description"],
+            "property_type": "room",
+            "transaction_type": "rent",
+            "price": float(self.parse_price(adapter["price"])),
+            "province": self.parse_address(adapter["address"])["province"],
+            "district": self.parse_address(adapter["address"])["district"],
+            "ward": self.parse_address(adapter["address"])["ward"],
+            "street": self.parse_address(adapter["address"])["street"],
+            "location_latitude": adapter["coordinates"][0],
+            "location_longitude": adapter["coordinates"][1],
+            "owner_name": adapter["owner_name"],
+            "owner_contact": self.parse_phone_number(adapter["owner_contact"]),
+            "area": self.parse_area(adapter["area"]),
+            "bedrooms": adapter["bedrooms"],
+            "bathrooms": adapter["bathrooms"],
+        }
+
+        print(f"Writing to csv: {json_data}")
+        self.dict_writer.writerow(json_data)
+        print(f"Done writing to csv")
+        return item
+
+    def close_spider(self, spider):
+        self.file.close()
--- website_scraper/settings.py
+++ website_scraper/settings.py
@@ -1,5 +1,6 @@
 SPIDER_MODULES = ["website_scraper.spiders"]
 
 ITEM_PIPELINES = {
-    "website_scraper.pipelines.MogiPipeline": 300,
+    # "website_scraper.pipelines.MogiPipeline": 300,
+    "website_scraper.pipelines.CSVExportPipeline": 400,
 }
